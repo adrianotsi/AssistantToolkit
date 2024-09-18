d810aaacf9b9bf3e4f48a5e12c9b0866df4966cb
Adriano Vieira <tsiadriano@gmail.com>
2024-09-18 18:29:26 -0300

    [DOC] Openai tags

--- src/index.py
+++ src/index.py
@@ -4,22 +4,26 @@ from fastapi import FastAPI
 from services.get_LLMResponse import get_LLMResponse, DiscoveryContext
 from services.query_discovery import query_discovery, UserQuery
 
-app = FastAPI()
+app = FastAPI(
+    title="Assistant Toolkit",
+    description="A short API to integrate IBM Discovery plus LLM Models.",
+    version="1.0.0"
+)
 load_dotenv()
 
 
-@app.get("/")
+@app.get("/", tags=["Hello World"], name='its alive?', description='Just a Hello World')
 def read_root():
     return {"Hello": "World"}
 
 
-@app.post("/queryDiscovery/")
+@app.post("/queryDiscovery/", tags=["Query Search"])
 async def queryDiscovery(request: UserQuery):
     print(request)
     res = query_discovery(request.input)
     return res
 
-@app.post("/getLLMResponse/")
+@app.post("/getLLMResponse/", tags=["LLM"])
 async def getLLMResponse(request: DiscoveryContext):
     res = get_LLMResponse(request)
     return res
