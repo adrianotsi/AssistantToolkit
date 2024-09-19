ab0b868101276b640da57859ed5c13bf523c59b8
Adriano Vieira <tsiadriano@gmail.com>
2024-09-19 16:57:56 -0300

    [REFACT] Change prompt to LLM

--- services/get_LLMResponse.py
+++ services/get_LLMResponse.py
@@ -13,9 +13,13 @@ def get_LLMResponse(discovery_context):
         'Content-Type': 'application/json'
     }
 
-    payload = { 
-        "model": "llama3.1:70b",
-        "prompt": f"""
+    payload = {
+        "model": "llama3.1",
+        "stream": False,
+        "messages": [
+            {
+                "role": "system",
+                "content": f"""
                     You are a virtual assistant designed to assist agents during a customer retention operation. 
                     Your primary role is to instruct the agent on how to retain the customer by offering all possible retention strategies. 
                     When a customer expresses the desire to cancel their plan, guide the agent to propose alternative offers or solutions to prevent cancellation. 
@@ -23,24 +27,23 @@ def get_LLMResponse(discovery_context):
                     Always provide the agent with specific instructions on what to say and do. Do not speak directly to the customer. 
                     Instead, instruct the agent on how to approach the conversation and retain the customer as much as possible. 
                     All responses must always be provided in **Brazilian Portuguese**. 
-                    This is your knowledgebase: {discovery_context.context}.
-                    This is the question from agent: {discovery_context.question}""",
-        "stream":False
+                    This is your knowledgebase: {discovery_context.context}."""
+            },
+            {
+                "role": "user",
+                "content": discovery_context.question
+            }
+        ]
     }
 
-    # TODO: improve logic to handle LLM response
     try:
-        response = requests.post(os.getenv("LLAMA_API"), headers=headers, json=payload)
+        response = requests.post(f"{os.getenv("LLAMA_API")}/api/chat", headers=headers, json=payload)
         response.raise_for_status()
 
         data = response.json()
-        print(data)
-        if 'text' in data:
-            return data['text']
-        elif 'choices' in data and len(data['choices']) > 0:
-            return data['choices'][0]['text']
+        if data["message"]["content"]:
+            return data["message"]["content"]
         else:
-            return data
+            return "Não encontrei nada a respeito, pode perguntar novamente?"
     except requests.exceptions.RequestException as e:
-        print('Erro ao chamar a API do Llama:', e)
         raise HTTPException(status_code=500, detail="Erro ao obter resposta do Llama" + str(e))
