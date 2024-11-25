5d84c29edf1158a3e4bd0bd944710a2ffc96367b
Adriano Vieira <tsiadriano@gmail.com>
2024-11-25 20:30:31 -0300

    [CHORE] Change version number

--- src/index.py
+++ src/index.py
@@ -14,7 +14,7 @@ from services.mongo_service import MongoService
 app = FastAPI(
     title="Assistant Toolkit",
     description="An API to integrate IBM Discovery with LLM Models and A.I Assistants.",
-    version="2.0.1"
+    version="2.0.2"
 )
 
 app.openapi_version = "3.0.2"
