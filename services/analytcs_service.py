58b62b7fae4a93b87d35f68d21b4e8e720573fef
Adriano Vieira <tsiadriano@gmail.com>
2024-12-11 20:05:22 -0300

    [REFACT] Now the report get a XLSX file

--- services/analytcs_service.py
+++ services/analytcs_service.py
@@ -54,16 +54,16 @@ class AnalytcsService:
                     df[column] = pd.to_numeric(df[column], errors='coerce') 
                     df[column] = (df[column] / 1_000_000_000).apply(lambda x: round(x, 2) if pd.notnull(x) else x)
 
-            # Gerando o arquivo CSV
-            csv_buffer = io.StringIO()
-            df.to_csv(csv_buffer, index=False)
-            csv_buffer.seek(0)
+            # Gerando o arquivo Excel
+            excel_buffer = io.BytesIO()
+            df.to_excel(excel_buffer, index=False, engine='openpyxl')
+            excel_buffer.seek(0)
 
             # Retornando o arquivo como streaming
             return StreamingResponse(
-                iter([csv_buffer.getvalue().encode('utf-8')]),
-                media_type="text/csv",
-                headers={"Content-Disposition": "attachment; filename=relatorio.csv"}
+                iter([excel_buffer.getvalue()]),
+                media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                headers={"Content-Disposition": "attachment; filename=relatorio.xlsx"}
             )
         
         except HTTPException as e:
